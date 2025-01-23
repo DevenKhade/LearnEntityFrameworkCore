08003e54bc56aa607f1e3d2d8f4c1086b08ef67f
Deven Khade <devenrkhade@gmail.com>
2025-01-23 14:06:34 +0530

    Initiallization of project

---
+++ EFCoreWebApi/EFCoreWebApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebApi.Data
+{
+    public class AppDbContext :DbContext
+    {
+        // make constructore of class and give them options value from  child to parent
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+
+        }
+    }
+}
